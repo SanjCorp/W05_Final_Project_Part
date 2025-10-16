5b95b842cf0286c8fb34a9ff0dcd62f37c0bc893
ricardo <richisanj@gmail.com>
2025-10-16 09:24:47 -0400

    Fix: added Customer model

--- models/customerModel.js
+++ models/customerModel.js
@@ -1,9 +1,23 @@
+// models/Customer.js
 const mongoose = require('mongoose');
 
 const customerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: String
-});
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    trim: true
+  },
+}, { timestamps: true });
 
 module.exports = mongoose.model('Customer', customerSchema);
