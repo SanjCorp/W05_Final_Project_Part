f07d8182fb113fed2af300409722764cadb9c6a0
ricardo <richisanj@gmail.com>
2025-10-16 13:45:26 -0400

    Fix: update controllers and routes to match model filenames

--- auth.js
+++ auth.js
@@ -6,7 +6,9 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.NODE_ENV === "production"
+        ? "https://w05-final-project-part.onrender.com/auth/google/callback"
+        : "http://localhost:3000/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => done(null, profile)
   )
